b1e2831213cb2ef75c92190b8eec72917855b101
Mihir Laddha <mihirladdha878@gmail.com>
2021-03-04 13:50:12 +0530

    Trailer popups added

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,7 @@
 import React,{useState,useEffect} from 'react';
+import Youtube from 'react-youtube';
 import axios from './axios';
+import movieTrailer from "movie-trailer";
 import './Row.css';
 
 const baseImgUrl = "https://image.tmdb.org/t/p/original";
@@ -7,6 +9,8 @@ const baseImgUrl = "https://image.tmdb.org/t/p/original";
 function Row({title,fetchUrl,isLargeRow}){
 
     const [movies,setMovies] = useState([]);
+
+    const [trailerIdUrl,setTrailerIdUrl] = useState("");
 
     useEffect(()=>{
         //if [] run once when row loads and don't run again
@@ -21,8 +25,30 @@ function Row({title,fetchUrl,isLargeRow}){
 
     },[fetchUrl]);
 
-    console.log(movies);
+    const opts = {
+      height : "390",
+      width : "100%",
+      playerVars : {
+        autoplay : "1",
+      },
+    };
 
+    const handleClick = (movie) => {
+        if(trailerIdUrl){
+            setTrailerIdUrl("");
+        }
+        else{
+            movieTrailer(movie?.title||movie?.name||"")
+            .then(url => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerIdUrl(urlParams.get("v"));                
+            })
+            .catch((error) => console.log(error));
+
+        }
+    }
+
+    //console.log(movies);
     return (
         <div className="row">
 
@@ -34,12 +60,17 @@ function Row({title,fetchUrl,isLargeRow}){
                 { movies.map(movie => (
                     <img 
                     key = {movie.id}
+                    onClick = { () => handleClick(movie)}
                     className = {`row__poster ${isLargeRow && "row__LargeRow"}`}
                     src = {`${baseImgUrl}${isLargeRow?movie.poster_path:movie.backdrop_path}`} alt = {movie.title} />
                 ))}
 
 
             </div>
+
+            <div>
+                {trailerIdUrl &&  <Youtube videoId={trailerIdUrl} opts={opts} />
+            }</div>
 
         </div>
     )
